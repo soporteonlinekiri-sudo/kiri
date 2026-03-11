2bd67f66633be87993322347a7abcfd54d925ffc
soporteonlinekiri-sudo <soporteonline.kiri@gmail.com>
2026-03-11 19:50:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kiri-v2';
-const ASSETS = ['/kiri/', '/kiri/index.html', '/kiri/manifest.json', '/kiri/icon-192.svg', '/kiri/icon-512.svg'];
+const CACHE = 'kiri-v3';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
